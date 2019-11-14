84a66b767b8d4b16332de93f535c3a1d413c30d2
Unknown <schroedersarah94@gmail.com>
2019-11-14 15:02:47 -0500

    Top streamer video playing
    added scrolling functionality to game data dialog

--- twitch-exposed-app/src/components/GameData.tsx
+++ twitch-exposed-app/src/components/GameData.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import '../components/GameData.css';
 import api from 'twitch-api-v5';
+import { TopStreamer } from '../components/TopStreamer';
 
 export interface GameDataProps{
     gameName: string;
@@ -26,14 +27,14 @@ export class GameData extends React.Component <GameDataProps, { streamData: any[
     api.streams.live({game: "League of Legends"}, (error, results) => {
         console.log(results);
         this.setState({
-            streamData:results.streams //BUG FIXED! Needed "results.streams" so rendering loop will actually work (lol)
+            streamData:results.streams
         })
     });
    }
 
     render() {        
         return (
-            <div>
+            <div className="game-data">
                 <div>{this.props.gameName}</div>
                 
                 <div>Total Viewer Count: {this.props.viewerCount}</div>
@@ -42,14 +43,24 @@ export class GameData extends React.Component <GameDataProps, { streamData: any[
                 { this.state.streamData
                     .sort((stream1, stream2) => (stream2.viewers > stream1.viewers) ? 1 : -1)
                     .slice(0, 10)
-                    .map( (stream, index) => <div>{ index + 1 }   <span>{ stream.viewers }</span>   <a href={stream.channel.url} target="_blank" >{ stream.channel.display_name }</a></div>)
+                    .map( (stream, index) => <TopStreamer 
+                                                streamerIndex={index + 1}
+                                                streamerCount={ stream.viewers }
+                                                streamerDisplayName={ stream.channel.display_name }
+                                                streamerUrl={stream.channel.url}
+                                            />)
                 }
                 <div>Top 10 streams by Follower Count: </div>
                 {
                     this.state.streamData
                     .sort((stream1, stream2) => (stream2.viewers > stream1.channel.followers) ? 1 : -1)
                     .slice(0, 10)
-                    .map( (stream, index) => <div>{ index + 1 }   <span>{ stream.channel.followers }</span>   <a href={stream.channel.url} target="_blank">{ stream.channel.display_name }</a></div>)
+                    .map( (stream, index) => <TopStreamer 
+                                                streamerIndex={index + 1}
+                                                streamerCount={ stream.channel.followers }
+                                                streamerDisplayName={ stream.channel.display_name }
+                                                streamerUrl={stream.channel.url}
+                                            />)
                 }
             </div>)
     }
---
+++ twitch-exposed-app/src/components/TopStreamer.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import ReactPlayer from 'react-player';
+
+export interface TopStreamerProps {
+    streamerIndex: number;
+    streamerCount: number; //the count of viewers or followers
+    streamerDisplayName: string;
+    streamerUrl: string;
+}
+
+export class TopStreamer extends React.Component<TopStreamerProps, { }> {
+    render() {
+        if(this.props.streamerIndex == 1){
+            return(
+                <div>
+                    { this.props.streamerIndex }.
+
+                    <ReactPlayer 
+                        url={ this.props.streamerUrl } 
+                        playing 
+                        width="640px"
+                        height="320px"
+                        allowfullscreen="false"
+                        muted
+                    />
+
+                    <span>{ this.props.streamerCount }</span>
+                    <a href={ this.props.streamerUrl } target="_blank" >{ this.props.streamerDisplayName }</a>
+                </div>
+            );
+        }
+        else {
+            return(
+                <div>
+                    { this.props.streamerIndex }.
+                    <span>{ this.props.streamerCount }</span>
+                    <a href={ this.props.streamerUrl } target="_blank" >{ this.props.streamerDisplayName }</a>
+                </div>
+            );
+        }
+        
+    }
+}
+
+export default TopStreamer;
